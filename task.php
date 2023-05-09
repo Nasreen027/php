3db4455b224b7fa18ad56da8667c4843419dccfb
Nasreen <nasreenmalik429@gmail.com>
2023-05-09 03:25:14 -0700

    Update task.php

--- task.php
+++ task.php
@@ -8,10 +8,11 @@ $b = $c;
 // echo "a value is " .$a ."<br>" . "b value is " .$b ."<br>";
 ?>
 <?php
-$a = 30;
-$b = 50;
+$a = 10;
+$b = 20;
 echo "a value is ".$a . "<br>" . "b value is " .$b ."<br>";
-$a = $b;
-$b = $a;
+$a = $a+$b;
+$b = $a-$b;
+$a = $a-$b;
 echo "a value is " .$a ."<br>" . "b value is " .$b;
 ?> 
