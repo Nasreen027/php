8308360632a753579655e1de1c90957b495e56fa
iman <nasreenmalik429@gmail.com>
2023-05-11 16:57:40 +0500

    //

---
+++ loanScheme.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="" method="post">
+<div class="form-group">
+  <label for="">Name</label>
+  <input type="text" name="name" id="" class="form-control" placeholder="" aria-describedby="helpId">
+</div>
+<div class="form-group">
+  <label for="">age</label>
+  <input type="number" name="age" id="" class="form-control" placeholder="" aria-describedby="helpId">
+
+</div>
+<div class="form-group">
+  <label for="">salary</label>
+  <input type="number" name="salary" id="" class="form-control" placeholder="" aria-describedby="helpId">
+
+</div>
+<div class="form-group">
+  <label for="">Loan Amount</label>
+  <input type="number" name="l_amount" id="" class="form-control" placeholder="100000 to 400000" aria-describedby="helpId">
+
+</div>
+<div class="form-group">
+  <label for="">Advanced payment</label>
+  <input type="number" name="a_payment" id="" class="form-control" placeholder="" aria-describedby="helpId">
+
+</div>
+<div class="form-group">
+  <label for="">Select Number of year</label>
+  <select class="form-control" name="year" id="">
+    <option value="">select</option>
+    <option>5</option>
+    <option>10</option>
+    <option>15</option>
+  </select>
+</div>
+<input type="submit" class="btn btn-dark" name="loan" value="get_loan">
+ 
+
+</form>
+
+</body>
+</html>
