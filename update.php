c95e899e3f752a78ce3e6c9fc4a819d8fdc761f3
Nasreen <nasreenmalik429@gmail.com>
2023-05-25 04:57:46 -0700

    Update update.php

--- update.php
+++ update.php
@@ -11,23 +11,28 @@ include('connection.php');
     <link rel="stylesheet" href="bootstrap-5/css/bootstrap.min.css">
 </head>
 <body>
+     <?php
+    if(isset($_GET["id"])){
+        $id = $_GET['id'];
+        $query = $pdo->prepare("SELECT * FROM products WHERE id=:id");
+        $query->bindParam("id",$id);
+        $query->execute();
+        $data = $query->fetch(PDO::FETCH_ASSOC);
+    }
+    ?>
 <div class="container">
 <form action="" method='post'>
     <div class="form-group">
-        <label for="">Id:</label>
-        <input type="text" name='getId' class="form-control">
-    </div>
-    <div class="form-group">
         <label for="">Name:</label>
-        <input type="text" name='getName' class="form-control">
+        <input type="text" value="<?php echo $data['name'] ?>" name='getName' class="form-control">
     </div>
     <div class="form-group">
         <label for="">Price:</label>
-        <input type="text" name='getPrice' class="form-control">
+        <input type="text" value="<?php echo $data['price'] ?>" name='getPrice' class="form-control">
     </div>
     <div class="form-group">
         <label for="">Quantity:</label>
-        <input type="text" name='getQuantity' class="form-control">
+        <input type="text" value="<?php echo $data['quantity'] ?>" name='getQuantity' class="form-control">
     </div>
     <button type='submit' name='updateBtn' class='btn btn-primary'>Update</button>
 </form>
@@ -35,12 +40,17 @@ include('connection.php');
 
 <?php
 if(isset($_POST['updateBtn'])){
-    $id = $_POST['getId'];
-    $name = $_POST['getName'];
-    $price = $_POST['getPrice'];
-    $quantity = $_POST['getQuantity'];
-    // $query = $pdo->
-};
+    $updateName = $_POST['getName'];
+    $updatePrice = $_POST['getPrice'];
+    $updateQuantity = $_POST['getQuantity'];
+    $query = $pdo->prepare("update products set name=:name, price=:price, quantity=:quantity where id=:id");
+    $query->bindParam('id',$id);
+    $query->bindParam('name',$updateName);
+    $query->bindParam('price',$updatePrice);
+    $query->bindParam('quantity',$updateQuantity);
+    $query->execute();
+    echo "<div class='alert alert-success'>Product updated</div>";
+}
 ?>
     
 </body>
